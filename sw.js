5150269936b5f3a37e57234e8fc71a26597a4343
cathy-zheng <59060962+cathy-zheng@users.noreply.github.com>
2026-07-24 20:58:16 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "cozy-yarn-tracker-v3";
+const APP_FILES = ["./", "./index.html", "./css/styles.css", "./js/app.js", "./manifest.webmanifest"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
+  );
+});
